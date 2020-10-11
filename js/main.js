8ac6dc2875ad80e7657ca1bc0ecad0a109d34d91
dmwebsit <72703983+dmwebsit@users.noreply.github.com>
2020-10-11 20:09:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,242 @@
+!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+   /* Записываем в переменные массив элементов-кнопок и подложку.
+      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+   var modalButtons = document.querySelectorAll('.modal_open'),
+       overlay      = document.querySelector('.js-overlay-modal'),
+       closeButtons = document.querySelectorAll('.js-modal-close');
+
+
+   /* Перебираем массив кнопок */
+   modalButtons.forEach(function(item){
+
+      /* Назначаем каждой кнопке обработчик клика */
+      item.addEventListener('click', function(e) {
+
+         /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+            Нужно подстраховаться. */
+         e.preventDefault();
+
+         /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+            и будем искать модальное окно с таким же атрибутом. */
+         var modalId = this.getAttribute('data-modal'),
+             modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+
+
+         /* После того как нашли нужное модальное окно, добавим классы
+            подложке и окну чтобы показать их. */
+         modalElem.classList.add('active');
+         overlay.classList.add('active');
+      }); // end click
+
+   }); // end foreach
+
+
+   closeButtons.forEach(function(item){
+
+      item.addEventListener('click', function(e) {
+         var parentModal = this.closest('.modal');
+
+         parentModal.classList.remove('active');
+         overlay.classList.remove('active');
+      });
+
+   }); // end foreach
+
+
+    document.body.addEventListener('keyup', function (e) {
+        var key = e.keyCode;
+
+        if (key == 27) {
+
+            document.querySelector('.modal.active').classList.remove('active');
+            document.querySelector('.overlay').classList.remove('active');
+        };
+    }, false);
+
+
+    overlay.addEventListener('click', function() {
+        document.querySelector('.modal.active').classList.remove('active');
+        this.classList.remove('active');
+    });
+
+
+
+
+}); // end ready
+
+//Accrodion
+$( function() {
+   $( "#accordion" ).accordion({
+     collapsible: true
+   });
+ } );
+
+ $( function() {
+   var icons = {
+     header: "ui-icon-circle-arrow-e",
+     activeHeader: "ui-icon-circle-arrow-s"
+   };
+   $( "#accordion" ).accordion({
+     icons: icons
+   }) 
+});
+
+// Arrow UP
+(function() {
+   'use strict';
+ 
+   function trackScroll() {
+     var scrolled = window.pageYOffset;
+     var coords = document.documentElement.clientHeight;
+ 
+     if (scrolled > coords) {
+       goTopBtn.classList.add('back_to_top-show');
+     }
+     if (scrolled < coords) {
+       goTopBtn.classList.remove('back_to_top-show');
+     }
+   }
+
+   function backToTop() {
+     if (window.pageYOffset > 0) {
+       window.scrollBy(0, -80);
+       setTimeout(backToTop, 0.30);
+     }
+   }
+ 
+   var goTopBtn = document.querySelector('.back_to_top');
+ 
+   window.addEventListener('scroll', trackScroll);
+   goTopBtn.addEventListener('click', backToTop);
+ })();
+
+// header scroll
+ jQuery(document).ready(function($){
+	var mainHeader = $('.header'),
+		secondaryNavigation = $('.cd-secondary-nav'),
+		//this applies only if secondary nav is below intro section
+		belowNavHeroContent = $('.sub-nav-hero'),
+		headerHeight = mainHeader.height();
+	
+	//set scrolling variables
+	var scrolling = false,
+		previousTop = 0,
+		currentTop = 0,
+		scrollDelta = 10,
+		scrollOffset = 150;
+
+	mainHeader.on('click', '.nav-trigger', function(event){
+		// open primary navigation on mobile
+		event.preventDefault();
+		mainHeader.toggleClass('nav-open');
+	});
+
+	$(window).on('scroll', function(){
+		if( !scrolling ) {
+			scrolling = true;
+			(!window.requestAnimationFrame)
+				? setTimeout(autoHideHeader, 250)
+				: requestAnimationFrame(autoHideHeader);
+		}
+	});
+
+	$(window).on('resize', function(){
+		headerHeight = mainHeader.height();
+	});
+
+	function autoHideHeader() {
+		var currentTop = $(window).scrollTop();
+
+		( belowNavHeroContent.length > 0 ) 
+			? checkStickyNavigation(currentTop) // secondary navigation below intro
+			: checkSimpleNavigation(currentTop);
+
+	   	previousTop = currentTop;
+		scrolling = false;
+	}
+
+	function checkSimpleNavigation(currentTop) {
+		//there's no secondary nav or secondary nav is below primary nav
+	    if (previousTop - currentTop > scrollDelta) {
+	    	//if scrolling up...
+	    	mainHeader.removeClass('is-hidden');
+	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
+	    	//if scrolling down...
+	    	mainHeader.addClass('is-hidden');
+	    }
+	}
+
+	function checkStickyNavigation(currentTop) {
+		//secondary nav below intro section - sticky secondary nav
+		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
+		
+		if (previousTop >= currentTop ) {
+	    	//if scrolling up... 
+	    	if( currentTop < secondaryNavOffsetTop ) {
+	    		//secondary nav is not fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('fixed slide-up');
+	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
+	    	} else if( previousTop - currentTop > scrollDelta ) {
+	    		//secondary nav is fixed
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+	    	
+	    } else {
+	    	//if scrolling down...	
+	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
+	 	  		//hide primary nav
+	    		mainHeader.addClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	} else if( currentTop > secondaryNavOffsetTop ) {
+	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
+	    		mainHeader.removeClass('is-hidden');
+	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
+	    		belowNavHeroContent.addClass('secondary-nav-fixed');
+	    	}
+
+	    }
+	}
+});
+
+
+new JustValidate('.footer__form', {
+   rules: {
+       name: {
+           required: true,
+           minLength: 2,
+           maxLength: 30
+       },            
+       mail: {
+           required: true,
+           email: true
+       },
+       message: {
+         required: true
+       }
+   
+   },
+   messages: {
+       name: {
+           required: 'Как вас зовут?',
+           minLength: 'Поле должно содержать минимум 2 символа'
+       },
+       tel: {
+           required: 'Укажите ваш телефон'
+       },
+       mail: {
+           required: 'Укажите ваш e-mail', 
+           email: 'Введите корректный e-mail'
+       }
+       },
+   colorWrong: '#ff6863'
+       
+});
